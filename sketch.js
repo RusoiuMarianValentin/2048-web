62c1f588b1b54e7da03da499b5327f28acca7da6
Valentin <marian.rusoiu99@e-uvt.ro>
2020-07-30 14:22:40 +0300

    modificat

--- sketch.js
+++ sketch.js
@@ -39,5 +39,32 @@ function addNumber(){
 }
 
 function draw(){
-    background(0);
+    background(255);
+    drawGrid(); 
+}
+
+function drawGrid(){
+
+    
+
+    let w = 100;
+
+    for(let i=0; i<4; i++){
+        for(let j=0;j<4;j++){
+            noFill();
+            strokeWeight(2);
+            stroke(0);
+            rect(i*w,j*w,w,w);
+            let val = grid[i][j];
+            if ( grid[i][j] !== 0 ){
+                textAlign(CENTER,CENTER);
+                textSize(64);
+                fill(0);
+                noStroke();
+                text(val, i*w + w/2, j*w + w/2 );
+
+            }
+
+        }
+    }
 }
